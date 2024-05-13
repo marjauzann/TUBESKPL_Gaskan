71ccaa4810f1d19328392f52886e0a387594afbb
Matthew Theodore Lempas <theodorelempas@gmail.com>
2024-05-13 22:34:02 +0700

    Progres 1 Theo

---
+++ Main_Sendit/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Sendit.Model
+{
+        public enum Role
+        {
+            Kurir,
+            Pengguna
+        }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string NamaLengkap { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string umur { get; set; }
+        public Role role { get; set; }
+
+        public User(string namaLengkap, string username, string password, string umur)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(namaLengkap));
+            Contract.Requires(!string.IsNullOrEmpty(username));
+            Contract.Requires(!string.IsNullOrEmpty(password));
+            Contract.Requires(!string.IsNullOrEmpty(umur));
+            Contract.Requires(umur.All(char.IsDigit));
+            Contract.Ensures(this.NamaLengkap == namaLengkap);
+            Contract.Ensures(this.userName == username);
+            Contract.Ensures(this.password == password);
+            Contract.Ensures(this.umur == umur);
+
+            Random random = new Random();
+            this.NamaLengkap = namaLengkap;
+            this.userName = username;
+            this.umur = umur;
+            this.Id = random.Next();
+            this.password = password;
+
+        }
+    }
+}
