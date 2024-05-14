deb4b95d0f7c90be43d29619a8a3677498c20192
Hasannp0611 <hasannp0611@gmail.com>
2024-05-14 14:15:40 +0700

    Update Table Driven

--- Main_Sendit/Table_Driven.cs
+++ Main_Sendit/Table_Driven.cs
@@ -3,25 +3,20 @@ namespace Main_Sendit
 {
 	public static class Table_Driven
     {
-
         public static string GetJarakdariUkuran(double Jauhjarak)
         {
-            string[] status = {"Sangat Jauh", "Jauh", "Sedang", "Dekat"};
-            double[] batasjarak = {20.0, 10.0, 5.0, 0.0};
-            int JarakMaksimal = status.Length - 1;
+            string[] status = { "Sangat Jauh", "Jauh", "Sedang", "Dekat" };
+            double[] batasjarak = { 20.0, 10.0, 5.0, 0.0 };
 
-            string StatusJarak = "Dekat";
-            double Jarak = 0;
-
-            while ((StatusJarak = "Dekat") && (Jarak < JarakMaksimal)) {
-                if (Jauhjarak > batasjarak[jarak])
+            for (int i = 0; i < batasjarak.Length; i++)
+            {
+                if (Jauhjarak > batasjarak[i])
                 {
-                    StatusJarak = status[Jarak];
+                    return status[i];
                 }
-                Jarak++;
             }
 
-            return StatusJarak;
+            return "Dekat";
         }
     }
 }
