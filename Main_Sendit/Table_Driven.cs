e1b3a28ce4c7e58179d8df99712916375d7efcfa
Hasannp0611 <hasannp0611@gmail.com>
2024-05-14 12:26:50 +0700

    Implementasi Table Driven

---
+++ Main_Sendit/Table_Driven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Main_Sendit
+{
+	public static class Table_Driven
+    {
+
+        public static string GetJarakdariUkuran(double Jauhjarak)
+        {
+            string[] status = {"Sangat Jauh", "Jauh", "Sedang", "Dekat"};
+            double[] batasjarak = {20.0, 10.0, 5.0, 0.0};
+            int JarakMaksimal = status.Length - 1;
+
+            string StatusJarak = "Dekat";
+            double Jarak = 0;
+
+            while ((StatusJarak = "Dekat") && (Jarak < JarakMaksimal)) {
+                if (Jauhjarak > batasjarak[jarak])
+                {
+                    StatusJarak = status[Jarak];
+                }
+                Jarak++;
+            }
+
+            return StatusJarak;
+        }
+    }
+}
+
