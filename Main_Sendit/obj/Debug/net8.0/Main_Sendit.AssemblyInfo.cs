65bc08203380621502110af866c4ccb706a07274
Matthew Theodore Lempas <99644704+viviole@users.noreply.github.com>
2024-06-07 18:49:17 +0700

    commit merge

--- Main_Sendit/obj/Debug/net8.0/Main_Sendit.AssemblyInfo.cs
+++ Main_Sendit/obj/Debug/net8.0/Main_Sendit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main_Sendit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f105c5edc43393f34e9fe6df97222a7a24e81ed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3905384f708e3a904c14eefcea6289c3fb68dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main_Sendit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main_Sendit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
